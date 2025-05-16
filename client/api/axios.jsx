a745fa56b36e5e2cbce5f35c93f24c6145290252
vanamuthuV <vanamuthuvana22@gmail.com>
2025-05-16 10:02:59 +0530

    urls updates from onrender

--- client/api/axios.jsx
+++ client/api/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const baseURL = "http://localhost:5000/api";
 
-const baseURL = "https://healthcare-app-sepia.vercel.app/api";
+const baseURL = "https://healthcareappserver.onrender.com/api";
 
 const api = axios.create({
   baseURL,
