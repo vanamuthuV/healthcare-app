fdafb14ba1ff54f9789f3cdadbec0ddd3956b816
vanamuthuV <vanamuthuvana22@gmail.com>
2025-05-16 09:34:25 +0530

    urls update

--- client/api/axios.jsx
+++ client/api/axios.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:5000/api";
+// const baseURL = "http://localhost:5000/api";
+
+const baseURL = "https://healthcare-app-sepia.vercel.app/api";
 
 const api = axios.create({
   baseURL,
--- server/src/config/cors.config.js
+++ server/src/config/cors.config.js
@@ -1,5 +1,5 @@
 export const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173", "https://healthcareapp-three.vercel.app/"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
 };
